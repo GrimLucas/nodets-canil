a0581aea25b801fe5dbdc6f6abb48054d3412b01
Lucas Martins <lucas666lp@gmail.com>
2021-10-01 11:14:41 -0300

    Projeto finalizado v1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -4,6 +4,10 @@ import {createMenuObject} from '../helpers/createMenuObject'
 
 export const search = (req:Request, res:Response)=>{
     let query: string = req.query.q as string;
+    if (!query) {
+        res.redirect('/')
+        return
+    }
     let list = Pet.getFromName(query);
     res.render('pages/page',{
         menu: createMenuObject(''),
